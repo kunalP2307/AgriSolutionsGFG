62072d6f756478509a54ded1d90ecb372926238b
sonusv7719 <sonu.s.v.771984@gmail.com>
2023-06-17 16:19:37 +0530

    Realtime_stream

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv');
+const { v4: uuidv4 } = require('uuid');
 const cors = require("cors");
 const db = require("./utils/db");
 const multer = require('multer');
@@ -8,6 +9,8 @@ const fs = require('fs');
 const uniqid = require('uniqid');
 const cloudinary = require('cloudinary').v2;
 const news_api = require('./utils/news_api');
+const http = require('http');
+const io = require('socket.io')(http);
 const app = express();
 app.use(express.json());
 
@@ -230,17 +233,17 @@ app.get('/find-latest/:collection_name/:limit', async (req, res) => {
             const collection = db.db.Connection.collection(collection_name);
             const collection_data = await collection.find().toArray();
             const reverse_collection = collection_data.reverse();
-            if(limit < reverse_collection.length){
-                for(let i=0; i<limit;i++){
+            if (limit < reverse_collection.length) {
+                for (let i = 0; i < limit; i++) {
                     final_data.push(reverse_collection[i]);
                 }
                 await db.db.disconnect();
                 res.status(200).json(final_data);
-            }else{
+            } else {
                 await db.db.disconnect();
                 res.status(200).json(reverse_collection);
             }
-            
+
         } else {
             await db.db.disconnect();
             res.json({ stat: 0, msg: "Collection not exists." });
@@ -366,6 +369,7 @@ app.post('/delete/:collection_name', async (req, res) => {
         let collection = await db.db.Connection.db.listCollections({ name: collection_name }).next();
         if (collection) {
             const collection = db.db.Connection.collection(collection_name);
+            console.log(doc['filter']);
             const collection_data = await collection.deleteOne(doc['filter']);
             console.log(collection_data)
             await db.db.disconnect();
@@ -421,40 +425,40 @@ async function buffer_to_image(buffer, outputPath, req, res) {
     const collection_name = req.params.collection_name;
     const doc = req.body;
     fs.writeFile(outputPath, buffer, (err) => {
-      if (err) {
-        console.error('Error writing image:', err);
-      } else {
-        cloudinary.uploader.upload(outputPath).then(async (result) => {
-            // console.log(result);
-            try {
-                doc['img_url'] = result.secure_url;
-                fs.unlinkSync(outputPath);
-                // console.log(doc)
-                await db.db.connect();
-                let collection = await db.db.Connection.db.listCollections({ name: collection_name }).next();
-                if (collection) {
-                    const collection = db.db.Connection.collection(collection_name);
-                    await collection.insertOne(doc);
-                    await db.db.disconnect();
-                    // res.status(200).json({ stat: 1, msg: "Done" });
-                    res.status(200).json({msg: "Data inserted with image", stat: 1, secure_url: result.secure_url, url: result.url, original_filename: result.original_filename, width:result.width, height: result.height, format: result.format, resourse_type: result.resource_type, bytes: result.bytes});
-                } else {
-                    await db.db.disconnect();
-                    res.json({ stat: 0, msg: "Collection not exists." });
+        if (err) {
+            console.error('Error writing image:', err);
+        } else {
+            cloudinary.uploader.upload(outputPath).then(async (result) => {
+                // console.log(result);
+                try {
+                    doc['img_url'] = result.secure_url;
+                    fs.unlinkSync(outputPath);
+                    // console.log(doc)
+                    await db.db.connect();
+                    let collection = await db.db.Connection.db.listCollections({ name: collection_name }).next();
+                    if (collection) {
+                        const collection = db.db.Connection.collection(collection_name);
+                        await collection.insertOne(doc);
+                        await db.db.disconnect();
+                        // res.status(200).json({ stat: 1, msg: "Done" });
+                        res.status(200).json({ msg: "Data inserted with image", stat: 1, secure_url: result.secure_url, url: result.url, original_filename: result.original_filename, width: result.width, height: result.height, format: result.format, resourse_type: result.resource_type, bytes: result.bytes });
+                    } else {
+                        await db.db.disconnect();
+                        res.json({ stat: 0, msg: "Collection not exists." });
+                    }
+
+                } catch (error) {
+                    res.json({ stat: 0, msg: error });
                 }
-                
-            } catch (error) {
-                res.json({stat:0, msg:error});
-            }
-            
-        }, (err)=>{
-            res.json({stat:0, msg:"Failed to upload"});
-        })
-        // console.log('Image successfully written to', outputPath);  
-      }
+
+            }, (err) => {
+                res.json({ stat: 0, msg: "Failed to upload" });
+            })
+            // console.log('Image successfully written to', outputPath);  
+        }
     });
 
-  }
+}
 
 
 
@@ -476,6 +480,93 @@ app.get('/scrap-news/:collection_name', (req, res) => {
     news_api(req, res);
 })
 
+// /--------------------------------------------------------------------------------------------\
+//                                  Realtime chatting API
+// \--------------------------------------------------------------------------------------------/
+
+const clients = new Map();
+
+app.get('/chat-updates/:collection_name', async (req, res) => {
+    const clientId = uuidv4();
+
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    res.flushHeaders();
+
+    clients.set(clientId, res);
+
+    req.on('close', () => {
+        clients.delete(clientId);
+    });
+
+    try {
+        const collection_name = req.params.collection_name;
+        await db.db.connect();
+        let collection = await db.db.Connection.db.listCollections({ name: collection_name }).next();
+        if (collection) {
+            const collection = db.db.Connection.collection(collection_name);
+            const collection_data = await collection.find().toArray();
+            await db.db.disconnect();
+
+            collection_data.forEach((message) => {
+                // res.write(`data: ${JSON.stringify(message)}\n\n`);
+                res.write(JSON.stringify(message));
+            });
+        } else {
+            await db.db.disconnect();
+            res.json({ stat: 0, msg: "Collection not exists." });
+        }
+       
+    } catch (error) {
+        console.error('Error fetching chat messages:', error);
+    }
+});
+
+
+app.post('/send-message/:collection_name', async (req, res) => {
+    try {
+        const collection_name = req.params.collection_name;
+        const doc = req.body;
+        // console.log(doc);
+        await db.db.connect();
+        // console.log(doc);
+        let collection = await db.db.Connection.db.listCollections({ name: collection_name }).next();
+        if (collection) {
+            const collection = db.db.Connection.collection(collection_name);
+            await collection.insertOne(doc);
+            const collection_data = await collection.find().toArray();
+            await db.db.disconnect();
+            for (const [clientId, clientResponse] of clients) {
+                // clientResponse.write(`data: ${JSON.stringify(doc)}\n\n`);
+                clientResponse.write(JSON.stringify(doc));
+            }
+            res.sendStatus(200);
+            // res.status(200).json({ stat: 1, msg: "Done" });
+        } else {
+            await db.db.disconnect();
+            res.json({ stat: 0, msg: "Collection not exists." });
+        }
+
+        
+    } catch (error) {
+        console.error('Error saving chat message:', error);
+        res.sendStatus(500);
+    }
+});
+
+app.get('/close-connections', (req, res) => {
+    try {
+        for (const [clientId, clientResponse] of clients) {
+            clientResponse.end(); 
+            clients.delete(clientId); 
+        }
+        res.sendStatus(200);
+    } catch (error) {
+        console.error('Error closing connections:', error);
+        res.sendStatus(500);
+    }
+});
 
 app.get('/', async (req, res) => {
     res.status(200).send({ msg: "Connected to SIH Project!!" });
